fb4d2ec79bb7c93eef9f5a872f3f0c1e78d78484
Sean Dewar <seandewar@users.noreply.github.com>
2021-12-13 04:22:05 +0000

    nvim/lsp: skip lsp config module in conf.util.reload()

--- neovim/.config/nvim/lua/conf/util.lua
+++ neovim/.config/nvim/lua/conf/util.lua
@@ -5,7 +5,15 @@ local M = {}
 function M.reload()
   for module, _ in pairs(package.loaded) do
     local capture = module:match "^conf(.*)$"
-    if capture and (capture == "" or capture:match "%..+") then
+
+    -- NOTE: do not reload the lsp modules as it'll cause lspconfig's setup()
+    -- function to be called twice, which will define multiple FileType autocmds
+    -- to start the servers that can cause issues (duplicate messages, etc.)
+    if
+      capture
+      and (capture == "" or capture:match "%..+")
+      and not capture:match "%.lsp"
+    then
       package.loaded[module] = nil
     end
   end
