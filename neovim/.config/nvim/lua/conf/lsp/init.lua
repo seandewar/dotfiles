8ae1f553f92de5b362c5ac59a03e926b5abf7c63
Sean Dewar <seandewar@users.noreply.github.com>
2022-01-02 15:22:36 +0000

    nvim/lsp: debounce text changes (150ms)

--- neovim/.config/nvim/lua/conf/lsp/init.lua
+++ neovim/.config/nvim/lua/conf/lsp/init.lua
@@ -108,6 +108,7 @@ lspconfig.util.default_config = vim.tbl_extend(
   {
     on_attach = on_attach,
     capabilities = capabilities,
+    flags = { debounce_text_changes = 150 },
     handlers = {
       ["textDocument/hover"] = lsp.with(lsp.handlers.hover, {
         border = "single",
