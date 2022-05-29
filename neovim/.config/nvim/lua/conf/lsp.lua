1c7ba672497d494597fb688a16bd92d1de66e677
Sean Dewar <seandewar@users.noreply.github.com>
2022-05-29 18:32:37 +0100

    nvim/lsp: use nvim_buf_call in attach/detach
    Passing the buf handle around everywhere is annoying

--- neovim/.config/nvim/lua/conf/lsp.lua
+++ neovim/.config/nvim/lua/conf/lsp.lua
@@ -2,6 +2,8 @@ local api = vim.api
 local lsp = vim.lsp
 
 local util = require "conf.util"
+local bmap = util.bmap
+local bunmap = util.bunmap
 
 local M = {}
 
@@ -53,14 +55,14 @@ end
 
 local buf_old_opts = {}
 
-function M.bopt(buf, option, value)
+local function bopt(option, value)
+  local buf = api.nvim_get_current_buf()
   buf_old_opts[buf] = vim.tbl_extend(
     "keep",
     buf_old_opts[buf] or {},
-    { [option] = vim.bo[buf][option] }
+    { [option] = vim.bo[option] }
   )
-
-  vim.bo[buf][option] = value
+  vim.bo[option] = value
 end
 
 function M.attach_buffer(args)
@@ -69,40 +71,35 @@ function M.attach_buffer(args)
     return
   end
 
-  local function bopt(...)
-    M.bopt(args.buf, ...)
-  end
-  local function bmap(...)
-    util.bmap(args.buf, ...)
-  end
+  api.nvim_buf_call(args.buf, function()
+    bopt("omnifunc", "v:lua.vim.lsp.omnifunc")
+    bopt("tagfunc", "v:lua.vim.lsp.tagfunc")
+    bopt("formatexpr", "v:lua.vim.lsp.formatexpr()")
 
-  bopt("omnifunc", "v:lua.vim.lsp.omnifunc")
-  bopt("tagfunc", "v:lua.vim.lsp.tagfunc")
-  bopt("formatexpr", "v:lua.vim.lsp.formatexpr()")
+    bmap("n", "K", lsp.buf.hover, "LSP Hover")
+    bmap({ "n", "i" }, "<C-K>", lsp.buf.signature_help, "LSP Signature Help")
+    bmap("n", "gd", lsp.buf.definition, "LSP Goto Definition")
+    bmap("n", "gD", lsp.buf.declaration, "LSP Goto Declaration")
+    bmap("n", "<Space>t", lsp.buf.type_definition, "LSP Goto Type Definition")
+    bmap("n", "<Space>R", lsp.buf.rename, "LSP Rename")
+    bmap("n", "<Space>f", function()
+      lsp.buf.format { async = true }
+    end, "LSP Formatting")
+    bmap("n", "<Space>a", lsp.buf.code_action, "LSP Code Action")
+    bmap("x", "<Space>f", "<Esc><Cmd>lua vim.lsp.buf.range_formatting()<CR>")
+    bmap("x", "<Space>a", "<Esc><Cmd>lua vim.lsp.buf.range_code_action()<CR>")
 
-  bmap("n", "K", lsp.buf.hover, "LSP Hover")
-  bmap({ "n", "i" }, "<C-K>", lsp.buf.signature_help, "LSP Signature Help")
-  bmap("n", "gd", lsp.buf.definition, "LSP Goto Definition")
-  bmap("n", "gD", lsp.buf.declaration, "LSP Goto Declaration")
-  bmap("n", "<Space>t", lsp.buf.type_definition, "LSP Goto Type Definition")
-  bmap("n", "<Space>R", lsp.buf.rename, "LSP Rename")
-  bmap("n", "<Space>f", function()
-    lsp.buf.format { async = true }
-  end, "LSP Formatting")
-  bmap("n", "<Space>a", lsp.buf.code_action, "LSP Code Action")
-  bmap("x", "<Space>f", "<Esc><Cmd>lua vim.lsp.buf.range_formatting()<CR>")
-  bmap("x", "<Space>a", "<Esc><Cmd>lua vim.lsp.buf.range_code_action()<CR>")
-
-  -- telescope.nvim
-  bmap("n", "<Space>i", "<Cmd>Telescope lsp_implementations<CR>")
-  bmap("n", "<Space>r", "<Cmd>Telescope lsp_references<CR>")
-  bmap("n", "<Space>w", "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>")
-  bmap("n", "<Space>d", "<Cmd>Telescope lsp_document_symbols<CR>")
+    -- telescope.nvim
+    bmap("n", "<Space>i", "<Cmd>Telescope lsp_implementations<CR>")
+    bmap("n", "<Space>r", "<Cmd>Telescope lsp_references<CR>")
+    bmap("n", "<Space>w", "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>")
+    bmap("n", "<Space>d", "<Cmd>Telescope lsp_document_symbols<CR>")
+  end)
 end
 
 function M.lspconfig_attach_curbuf(client)
   if client.name == "clangd" then
-    util.bmap(0, "n", "<Space>s", "<Cmd>ClangdSwitchSourceHeader<CR>")
+    bmap("n", "<Space>s", "<Cmd>ClangdSwitchSourceHeader<CR>")
   end
 end
 
@@ -116,34 +113,32 @@ function M.detach_buffer(args)
     return
   end
 
-  local function bunmap(...)
-    util.bunmap(args.buf, ...)
-  end
+  api.nvim_buf_call(args.buf, function()
+    bunmap("n", "K")
+    bunmap({ "n", "i" }, "<C-K>")
+    bunmap("n", "gd")
+    bunmap("n", "gD")
+    bunmap("n", "<Space>t")
+    bunmap("n", "<Space>R")
+    bunmap("n", "<Space>f")
+    bunmap("n", "<Space>a")
+    bunmap("x", "<Space>f")
+    bunmap("x", "<Space>a")
 
-  bunmap("n", "K")
-  bunmap({ "n", "i" }, "<C-K>")
-  bunmap("n", "gd")
-  bunmap("n", "gD")
-  bunmap("n", "<Space>t")
-  bunmap("n", "<Space>R")
-  bunmap("n", "<Space>f")
-  bunmap("n", "<Space>a")
-  bunmap("x", "<Space>f")
-  bunmap("x", "<Space>a")
+    -- telescope.nvim
+    bunmap("n", "<Space>i")
+    bunmap("n", "<Space>r")
+    bunmap("n", "<Space>w")
+    bunmap("n", "<Space>d")
 
-  -- telescope.nvim
-  bunmap("n", "<Space>i")
-  bunmap("n", "<Space>r")
-  bunmap("n", "<Space>w")
-  bunmap("n", "<Space>d")
+    -- lspconfig's on_attach (may not be defined)
+    pcall(bunmap, "n", "<Space>s")
 
-  -- lspconfig's on_attach (may not be defined)
-  pcall(bunmap, "n", "<Space>s")
-
-  for option, old_value in ipairs(buf_old_opts[args.buf] or {}) do
-    vim.bo[args.buf][option] = old_value
-  end
-  buf_old_opts[args.buf] = nil
+    for option, old_value in ipairs(buf_old_opts[args.buf] or {}) do
+      vim.bo[option] = old_value
+    end
+    buf_old_opts[args.buf] = nil
+  end)
 end
 
 return M
