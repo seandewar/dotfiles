8d331a82ad3e3203f22fbcc53a8e8fc18ac65608
Sean Dewar <seandewar@users.noreply.github.com>
2023-10-02 22:29:43 +0100

    nvim: remove util.t (vim.keycode() exists now)

--- neovim/.config/nvim/lua/conf/util.lua
+++ neovim/.config/nvim/lua/conf/util.lua
@@ -2,11 +2,6 @@ local api = vim.api
 
 local M = {}
 
---- replaces termcodes in a string
-function M.t(str)
-  return api.nvim_replace_termcodes(str, true, true, true)
-end
-
 function M.echo(msg)
   msg = type(msg) == "string" and { { msg } } or msg
   api.nvim_echo(msg, false, {})
