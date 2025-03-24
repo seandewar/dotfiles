984e2bd652ed6ad16fe23bbc153291b1d5b9372e
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2025-03-24 23:22:25 +0000

    nvim: update

--- neovim/.config/nvim/plugin/fzf.lua
+++ neovim/.config/nvim/plugin/fzf.lua
@@ -40,19 +40,9 @@ fzf.setup {
   },
 
   fzf_colors = {
-    ["fg"] = { "fg", "CursorLine" },
-    ["bg"] = { "bg", "Normal" },
-    ["hl"] = { "fg", "Comment" },
-    ["fg+"] = { "fg", "Normal" },
-    ["bg+"] = { "bg", "CursorLine" },
-    ["hl+"] = { "fg", "Statement" },
-    ["info"] = { "fg", "PreProc" },
-    ["prompt"] = { "fg", "Conditional" },
-    ["pointer"] = { "fg", "Exception" },
-    ["marker"] = { "fg", "Keyword" },
-    ["spinner"] = { "fg", "Label" },
-    ["header"] = { "fg", "Comment" },
-    ["gutter"] = { "bg", "Normal" },
+    true,
+    ["hl"] = { "bg", "Search" },
+    ["hl+"] = { "bg", "CurSearch" },
   },
 }
 fzf.register_ui_select()
